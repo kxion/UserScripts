cb46815c93a62ceea4233b890c4b18206608be06
ytzong <ytzong7@gmail.com>
2016-05-06 15:56:05 +0800

    bug fixes

--- 91porn-html5-player.user.js
+++ 91porn-html5-player.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         91Porn HTML5 Player
-// @version      0.8
+// @version      0.9
 // @author       ytzong
 // @description  91Porn
 // @include      http://*91*.space/*
@@ -10,7 +10,7 @@
 // @grant        GM_addStyle
 // ==/UserScript==
 
-GM_addStyle('body{width:100%;overflow-x:hidden;}#container, #container table, #container_video table {width:100% !important}#container td[align="right"],#container td[align="left"],#container_video > table > tbody > tr > td:nth-child(1),#container_video > table > tbody > tr > td:nth-child(3),#rightside, .arrow-general, #topbar{display:none !important}#leftside{width:100% !important} #recently, #userinfo, #mediumbox, #mostactive, #topwatched, #signup, #browsegroup, #viewvideo, #recently-added, #myvideo, #myfriends, #groups, #bookmark, #videodetails, #sharedetails, #videocomment,#fullside, #fullbox, #invitefriend, #invitenewfriend,#paging,.pagingnav,#submenu, #subcontent {width:auto !important}.imagechannelhd{width:auto !important;height:auto !important}.pagingnav a, span.pagingnav{padding: 10px 20px !important;margin:6px !important}input.page_number {margin: 6px !important;padding: 9px !important;}.listchannel{text-align:left !important; width:210px !important;height:210px !important;} .imagechannel a img, .imagechannelhd a img,#subcontent p a img {height: 117px !important;width: 208px !important;}#viewvideo-content,.videoplayer{padding:0 !important}.videoplayer{margin:0 !important}#subcontent{overflow:hidden;}#subcontent p, #subcontent p.blue{float:none !important;display: inline-block !important;width: 444px !important;vertical-align: top;}#tab-featured a[href="video.php?category=rf"]{display: block;text-align: center;padding: 6px;}#containersearch{margin:10px !important;}#search{display:inline-block;}#videodetails{width:500px !important;}#useraction{position:relative;left:50%;margin-left:-225px;}#viewvideo{border:0 none !important;}#viewvideo-title{background-image: none !important;text-align:center !important;}');
+GM_addStyle('body{width:100%;overflow-x:hidden;}#container, #container table, #container_video table {width:100% !important}#container td[align="right"],#container td[align="left"],#container_video > table > tbody > tr > td:nth-child(1),#container_video > table > tbody > tr > td:nth-child(3),#rightside, .arrow-general, #topbar{display:none !important}#leftside{width:100% !important} #recently, #userinfo, #mediumbox, #mostactive, #topwatched, #signup, #browsegroup, #viewvideo, #recently-added, #myvideo, #myfriends, #groups, #bookmark, #videodetails, #sharedetails, #videocomment,#fullside, #fullbox, #invitefriend, #invitenewfriend,#paging,.pagingnav,#submenu, #subcontent {width:auto !important}.imagechannelhd{width:auto !important;height:auto !important}.pagingnav a, span.pagingnav{padding: 10px 20px !important;margin:6px !important}input.page_number {margin: 6px !important;padding: 9px !important;}.listchannel, .videothumb{text-align:left !important; width:210px !important;height:210px !important;} .imagechannel a img, .imagechannelhd a img,#subcontent p a img,.videothumb img {padding:0 !important;height: 117px !important;width: 208px !important;}#viewvideo-content,.videoplayer{padding:0 !important}.videoplayer{margin:0 !important}#subcontent{overflow:hidden;}#subcontent p, #subcontent p.blue{float:none !important;display: inline-block !important;width: 444px !important;vertical-align: top;}#tab-featured a[href="video.php?category=rf"]{display: block;text-align: center;padding: 6px;}#containersearch{margin:10px !important;}#search{display:inline-block;}#videodetails{width:500px !important;}#useraction{position:relative;left:50%;margin-left:-225px;}#viewvideo{border:0 none !important;}#viewvideo-title{background-image: none !important;text-align:center !important;}.myvideo{float:none!important;display:inline-block !important;width:49% !important;vertical-align: top;}');
 
 window.setTimeout(YTPlay, 500);
 
@@ -51,9 +51,12 @@ function YTPlay(){
             console.log(str);
             var height = $(window).height();
             var width = $('.videoplayer').width();
-            $('.videoplayer').html('<video id="yt-video" src="' + str + '" controls autoplay style="width:' + width + 'px; height:' + height + 'px"></video><p style="text-align:center"></p>');
+            $('.videoplayer').html('<video id="yt-video" src="' + str + '" controls autoplay loop style="width:' + width + 'px; height:' + height + 'px"></video><p style="text-align:center"></p>');
             $('#viewvideo-title').html('<a id="yt-download" href="' + str + '" download="' + title + '.mp4">' + title + '</a>');
             $('#rightside').parent().attr('width', '0');
+            $("video").on("error", function(err) {
+                location.reload(true);
+            });
         });
     }
     function rotate(deg) {
@@ -70,38 +73,47 @@ function YTPlay(){
     }
     var degree = 0;
     $(document).keydown(function(e) {
+        var video = $('video')[0];
+        //video.attr('controls', 'controls');
         //R
-        if(e.keyCode == 82) {
+        if (e.keyCode == 82) {
             degree += 90;
             rotate(degree);
         }
         //D
-        if(e.keyCode == 68) {
+        if (e.keyCode == 68) {
             $('#yt-download').get(0).click();
         }
         //P
-        if(e.keyCode == 80) {
-            var video = $('video').get(0);
+        if (e.keyCode == 80) {
             if (video.paused) video.play();
             else video.pause();
         }
         //右箭头
-        if(e.keyCode == 39) {
-            var video = $('video').get(0);
-            video.currentTime = video.currentTime + 10;
+        if (e.keyCode == 39) {
+            if (e.metaKey) video.volume = video.volume + 0.1;
+            else video.currentTime = video.currentTime + 10;
         }
         //左箭头
-        if(e.keyCode == 37) {
-            var video = $('video').get(0);
-            video.currentTime = video.currentTime - 5;
+        if (e.keyCode == 37) {
+            if (e.metaKey) video.volume = 0.1;
+            else video.currentTime = video.currentTime - 10;
+        }
+        //ALT + 右箭头
+        if (e.altKey && e.keyCode == 39) {
+            video.currentTime = video.currentTime + 60;
+        }
+        //ALT + 左箭头
+        if (e.altKey && e.keyCode == 37) {
+            video.currentTime = video.currentTime - 60;
         }
         /*
         //Q
-        if(e.keyCode == 81) {
+        if (e.keyCode == 81) {
             self.location = $('span.pagingnav').next().attr('href');
         }
         //W
-        if(e.keyCode == 87) {
+        if (e.keyCode == 87) {
             self.location = $('span.pagingnav').prev().attr('href');
         }
         */
