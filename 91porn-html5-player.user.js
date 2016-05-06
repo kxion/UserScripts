c570d08da1da5080a48d6cbc68e922074669f901
ytzong <ytzong7@gmail.com>
2016-05-06 13:32:06 +0800

    bug fixes

--- 91porn-html5-player.user.js
+++ 91porn-html5-player.user.js
@@ -1,15 +1,16 @@
 // ==UserScript==
 // @name         91Porn HTML5 Player
-// @version      0.7
+// @version      0.8
 // @author       ytzong
 // @description  91Porn
-// @include      http://*.space/*
+// @include      http://*91*.space/*
+// @include      http://*9p1*.space/*
 // @copyright    2016+
 // @run-at       document-idle
 // @grant        GM_addStyle
 // ==/UserScript==
 
-GM_addStyle('#container, #container table, #container_video table {width:100% !important}#container td[align="right"],#container td[align="left"],#container_video > table > tbody > tr > td:nth-child(1),#container_video > table > tbody > tr > td:nth-child(3),#rightside, .arrow-general, #topbar{display:none !important}#leftside{width:100% !important} #recently, #userinfo, #mediumbox, #mostactive, #topwatched, #signup, #browsegroup, #viewvideo, #recently-added, #myvideo, #myfriends, #groups, #bookmark, #videodetails, #sharedetails, #videocomment,#fullside, #fullbox, #invitefriend, #invitenewfriend,#paging,.pagingnav,#submenu, #subcontent {width:auto !important}.imagechannelhd{width:auto !important;height:auto !important}.pagingnav a, span.pagingnav{padding: 10px 20px !important;margin:6px !important}input.page_number {margin: 6px !important;padding: 9px !important;}.listchannel{text-align:left !important; width:210px !important;height:210px !important;} .imagechannel a img, .imagechannelhd a img,#subcontent p a img {height: 117px !important;width: 208px !important;}#viewvideo-content,.videoplayer{padding:0 !important}.videoplayer{margin:0 !important}#subcontent{overflow:hidden;}#subcontent p, #subcontent p.blue{float:none !important;display: inline-block !important;width: 444px !important;vertical-align: top;}#tab-featured a[href="video.php?category=rf"]{display: block;text-align: center;padding: 6px;}#containersearch{margin:10px !important;}#search{display:inline-block;}#videodetails{width:500px !important;}#useraction{position:relative;left:50%;margin-left:-225px;}#viewvideo{border:0 none !important;}#viewvideo-title{background-image: none !important;text-align:center !important;}');
+GM_addStyle('body{width:100%;overflow-x:hidden;}#container, #container table, #container_video table {width:100% !important}#container td[align="right"],#container td[align="left"],#container_video > table > tbody > tr > td:nth-child(1),#container_video > table > tbody > tr > td:nth-child(3),#rightside, .arrow-general, #topbar{display:none !important}#leftside{width:100% !important} #recently, #userinfo, #mediumbox, #mostactive, #topwatched, #signup, #browsegroup, #viewvideo, #recently-added, #myvideo, #myfriends, #groups, #bookmark, #videodetails, #sharedetails, #videocomment,#fullside, #fullbox, #invitefriend, #invitenewfriend,#paging,.pagingnav,#submenu, #subcontent {width:auto !important}.imagechannelhd{width:auto !important;height:auto !important}.pagingnav a, span.pagingnav{padding: 10px 20px !important;margin:6px !important}input.page_number {margin: 6px !important;padding: 9px !important;}.listchannel{text-align:left !important; width:210px !important;height:210px !important;} .imagechannel a img, .imagechannelhd a img,#subcontent p a img {height: 117px !important;width: 208px !important;}#viewvideo-content,.videoplayer{padding:0 !important}.videoplayer{margin:0 !important}#subcontent{overflow:hidden;}#subcontent p, #subcontent p.blue{float:none !important;display: inline-block !important;width: 444px !important;vertical-align: top;}#tab-featured a[href="video.php?category=rf"]{display: block;text-align: center;padding: 6px;}#containersearch{margin:10px !important;}#search{display:inline-block;}#videodetails{width:500px !important;}#useraction{position:relative;left:50%;margin-left:-225px;}#viewvideo{border:0 none !important;}#viewvideo-title{background-image: none !important;text-align:center !important;}');
 
 window.setTimeout(YTPlay, 500);
 
@@ -50,7 +51,7 @@ function YTPlay(){
             console.log(str);
             var height = $(window).height();
             var width = $('.videoplayer').width();
-            $('.videoplayer').html('<video src="' + str + '" controls autoplay style="width:' + width + 'px; height:' + height + 'px"></video><p style="text-align:center"></p>');
+            $('.videoplayer').html('<video id="yt-video" src="' + str + '" controls autoplay style="width:' + width + 'px; height:' + height + 'px"></video><p style="text-align:center"></p>');
             $('#viewvideo-title').html('<a id="yt-download" href="' + str + '" download="' + title + '.mp4">' + title + '</a>');
             $('#rightside').parent().attr('width', '0');
         });
