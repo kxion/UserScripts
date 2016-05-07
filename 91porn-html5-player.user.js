0a58732080f35a25df33f4c81ad5ec25e000d3d2
ytzong <ytzong7@gmail.com>
2016-05-08 04:31:07 +0800

    bug fixes

--- 91porn-html5-player.user.js
+++ 91porn-html5-player.user.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name         91Porn HTML5 Player
-// @version      1.0
+// @version      1.1
 // @author       ytzong
 // @description  91Porn
-// @include      http://*91porn.com/*
+// @include      http://*91porn*/*
 // @include      http://*91*.space/*
 // @include      http://*9p1*.space/*
 // @copyright    2016+
@@ -11,7 +11,7 @@
 // @grant        GM_addStyle
 // ==/UserScript==
 
-GM_addStyle('body{width:100%;overflow-x:hidden;}#container, #container table, #container_video table {width:100% !important}#container td[align="right"],#container td[align="left"],#container_video > table > tbody > tr > td:nth-child(1),#container_video > table > tbody > tr > td:nth-child(3),#container_video #rightside, .arrow-general, #topbar, embed{display:none !important} #leftside{width:100% !important} #recently, #userinfo, #mediumbox, #mostactive, #topwatched, #signup, #browsegroup, #viewvideo, #recently-added, #myvideo, #myfriends, #groups, #bookmark, #videodetails, #sharedetails, #videocomment,#fullside, #fullbox, #invitefriend, #invitenewfriend,#paging,.pagingnav,#submenu, #subcontent {width:auto !important}.imagechannelhd{width:auto !important;height:auto !important}.pagingnav a, span.pagingnav{padding: 10px 20px !important;margin:6px !important}input.page_number {margin: 6px !important;padding: 9px !important;}.listchannel, .videothumb{text-align:left !important; width:210px !important;height:210px !important;} .imagechannel a img, .imagechannelhd a img,#subcontent p a img,.videothumb img {padding:0 !important;height: 117px !important;width: 208px !important;}#viewvideo-content,.videoplayer{padding:0 !important}.videoplayer{margin:0 !important}#subcontent{overflow:hidden;}#subcontent p, #subcontent p.blue{float:none !important;display: inline-block !important;width: 444px !important;vertical-align: top;}#tab-featured a[href="video.php?category=rf"]{display: block;text-align: center;padding: 6px;}#containersearch{margin:10px !important;}#search{display:inline-block;}#videodetails{width:500px !important;}#useraction{position:relative;left:50%;margin-left:-225px;}#viewvideo{border:0 none !important;}#viewvideo-title{background-image: none !important;text-align:center !important;}.myvideo{float:none!important;display:inline-block !important;width:49% !important;vertical-align: top;}video{margin:0 auto;}');
+GM_addStyle('body{width:100%;overflow-x:hidden;}#container, #container table, #container_video table, #hd_video {width:100% !important}#container td[align="right"],#container td[align="left"],#container_video > table > tbody > tr > td:nth-child(1),#container_video > table > tbody > tr > td:nth-child(3),#container_video #rightside, .arrow-general, #topbar, embed{display:none !important} #leftside{width:100% !important} #recently, #userinfo, #mediumbox, #mostactive, #topwatched, #signup, #browsegroup, #viewvideo, #viewvideo_hd, #recently-added, #myvideo, #myfriends, #groups, #bookmark, #videodetails, #sharedetails, #videocomment,#fullside, #fullbox, #invitefriend, #invitenewfriend,#paging,.pagingnav,#submenu, #subcontent {width:auto !important}.imagechannelhd{width:auto !important;height:auto !important}.pagingnav a, span.pagingnav{padding: 10px 20px !important;margin:6px !important}input.page_number {margin: 6px !important;padding: 9px !important;}.listchannel, .videothumb{text-align:left !important; width:210px !important;height:210px !important;} .imagechannel a img, .imagechannelhd a img,#subcontent p a img,.videothumb img {padding:0 !important;height: 117px !important;width: 208px !important;}#viewvideo-content,.videoplayer{padding:0 !important;background-image:none !important}.videoplayer{margin:0 !important}#subcontent{overflow:hidden;} /*#subcontent p, #subcontent p.blue{float:none !important;display: inline-block !important;width: 444px !important;vertical-align: top;} */#tab-featured a[href="video.php?category=rf"]{display: block;text-align: center;padding: 6px;}#containersearch{margin:10px !important;}#search{display:inline-block;}#videodetails{width:500px !important;}#useraction{position:relative;left:50%;margin-left:-225px;}#viewvideo, #viewvideo_hd{border:0 none !important;}#viewvideo-title{background-image: none !important;text-align:center !important;}.myvideo, .listchannel, .listchannellarge, #subcontent p, #subcontent p.blue{float:none!important;display:inline-block !important;width:49% !important;vertical-align: top;}video{margin:0 auto;}');
 
 window.setTimeout(YTPlay, 500);
 
@@ -41,7 +41,7 @@ function YTPlay(){
     $('#topbar').remove();
     var mp4 = 0;
     if( typeof(so) != 'undefined'){
-        if ($('#mediaspace img[src="images/hd.png"]').length > 0) mp4 = 1;
+        if ($('#mediaspace img[src="images/hd.png"]').length > 0 || $('#hd_video').length > 0) mp4 = 1;
         console.log(mp4);
         var title = $('#viewvideo-title').text().trim();
         var timestamp = $.now();
