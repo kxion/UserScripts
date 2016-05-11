efa0d0c1aed1d44e6b37eb9a2fc6742ea574643d
ytzong <ytzong7@gmail.com>
2016-05-11 15:54:31 +0800

    bug fixes

--- 91porn-html5-player.user.js
+++ 91porn-html5-player.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         91Porn HTML5 Player
-// @version      1.2
+// @version      1.3
 // @author       ytzong
 // @description  91Porn
 // @include      http://*91porn*/*
@@ -12,7 +12,11 @@
 // @require      https://cdnjs.cloudflare.com/ajax/libs/jquery-cookie/1.4.1/jquery.cookie.min.js
 // ==/UserScript==
 
-GM_addStyle('body{width:100%;overflow-x:hidden;}#container, #container table, #container_video table, #hd_video {width:100% !important}#container td[align="right"],#container td[align="left"],#container_video > table > tbody > tr > td:nth-child(1),#container_video > table > tbody > tr > td:nth-child(3),#container_video #rightside, .arrow-general, #topbar, embed{display:none !important} #leftside{width:100% !important} #recently, #userinfo, #mediumbox, #mostactive, #topwatched, #signup, #browsegroup, #viewvideo, #viewvideo_hd, #recently-added, #myvideo, #myfriends, #groups, #bookmark, #videodetails, #sharedetails, #videocomment,#fullside, #fullbox, #invitefriend, #invitenewfriend,#paging,.pagingnav,#submenu, #subcontent {width:auto !important}.imagechannelhd{width:auto !important;height:auto !important}.pagingnav a, span.pagingnav{padding: 10px 20px !important;margin:6px !important}input.page_number {margin: 6px !important;padding: 9px !important;}.listchannel, .videothumb{text-align:left !important; width:210px !important;height:210px !important;} .imagechannel a img, .imagechannelhd a img,#subcontent p a img,.videothumb img {padding:0 !important;height: 117px !important;width: 208px !important;}#viewvideo-content,.videoplayer{padding:0 !important;background-image:none !important}.videoplayer{margin:0 !important}#subcontent{overflow:hidden;}#tab-featured a[href="video.php?category=rf"]{display: block;text-align: center;padding: 6px;}#containersearch{margin:10px !important;}#search{display:inline-block;}#videodetails{width:500px !important;}#useraction{position:relative;left:50%;margin-left:-225px;}#viewvideo, #viewvideo_hd{border:0 none !important;}#viewvideo-title{background-image: none !important;text-align:center !important;}.myvideo, .listchannel, .listchannellarge, #subcontent p, #subcontent p.blue{float:none!important;display:inline-block !important;padding:10px 0.5%!important;vertical-align: top;}.myvideo, .listchannellarge, #subcontent p, #subcontent p.blue{width:48.5% !important;}.listchannel {}video{margin:0 auto;}');
+var pathnames = location.pathname.split('/');
+var pathname = pathnames[pathnames.length - 1];
+console.log(pathname);
+
+GM_addStyle('body{width:100%;overflow-x:hidden;}#paging{padding-bottom:250px}#container, #container table, #container_video table, #hd_video {width:100% !important}#container td[align="right"],#container td[align="left"],#container_video > table > tbody > tr > td:nth-child(1),#container_video > table > tbody > tr > td:nth-child(3),#container_video #rightside, .arrow-general, #topbar, embed{display:none !important} #leftside{width:100% !important} #recently, #userinfo, #mediumbox, #mostactive, #topwatched, #signup, #browsegroup, #viewvideo, #viewvideo_hd, #recently-added, #myvideo, #myfriends, #groups, #bookmark, #videodetails, #sharedetails, #videocomment,#fullside, #fullbox, #invitefriend, #invitenewfriend,#paging,.pagingnav,#submenu, #subcontent {width:auto !important}.imagechannelhd{width:auto !important;height:auto !important}.pagingnav a, span.pagingnav{padding: 10px 20px !important;margin:6px !important}input.page_number {margin: 6px !important;padding: 9px !important;}.listchannel, .videothumb{text-align:left !important; width:210px !important;height:210px !important;} .imagechannel a img, .imagechannelhd a img,#subcontent p a img,.videothumb img {padding:0 !important;height: 117px !important;width: 208px !important;}#viewvideo-content,.videoplayer{padding:0 !important;background-image:none !important}.videoplayer{margin:0 !important}#subcontent{overflow:hidden;}#tab-featured a[href="video.php?category=rf"]{display: block;text-align: center;padding: 6px;}#containersearch{margin:10px !important;}#search{display:inline-block;}#videodetails{width:500px !important;}#useraction{position:relative;left:50%;margin-left:-225px;}#viewvideo, #viewvideo_hd{border:0 none !important;}#viewvideo-title{background-image: none !important;text-align:center !important;}.myvideo, .listchannel, .listchannellarge, #subcontent p, #subcontent p.blue{float:none!important;display:inline-block !important;padding:10px 0.5%!important;vertical-align: top;}.myvideo, .listchannellarge, #subcontent p, #subcontent p.blue{width:48.5% !important;}.listchannel {}video{margin:0 auto;}');
 
 main();
 window.setTimeout(YTPlay, 500);
@@ -115,4 +119,52 @@ function YTPlay(){
         }
         */
     });
+}
+/*
+if ($('span.pagingnav').length > 0) {
+    //$(body).css('padding-bottom', '100px');
+    $(window).scroll(function() {
+       if($(window).scrollTop() + $(window).height() == $(document).height()) {
+           console.log("bottom!");
+           var next = $('span.pagingnav').next().attr('href');
+           location.href = next;
+       }
+    });
+}
+*/
+//http://joji.me/zh-cn/blog/how-to-develop-high-performance-onscroll-event
+if ($('span.pagingnav').length > 0) {
+    var next = $('span.pagingnav').next().attr('href');
+    var $window = $(window);
+    var $document = $(document);
+    var scroll = function () {
+        if($window.scrollTop() + $window.height() == $document.height()) {
+           location.href = next;
+        }
+    };
+    var raf = window.requestAnimationFrame ||
+        window.webkitRequestAnimationFrame ||
+        window.mozRequestAnimationFrame ||
+        window.msRequestAnimationFrame ||
+        window.oRequestAnimationFrame;
+    var $window = $(window);
+    var lastScrollTop = $window.scrollTop();
+
+    if (raf) {
+        loop();
+    }
+
+    function loop() {
+        var scrollTop = $window.scrollTop();
+        if (lastScrollTop === scrollTop) {
+            raf(loop);
+            return;
+        } else {
+            lastScrollTop = scrollTop;
+
+            // 如果进行了垂直滚动，执行scroll方法
+            scroll();
+            raf(loop);
+        }
+    }
 }
